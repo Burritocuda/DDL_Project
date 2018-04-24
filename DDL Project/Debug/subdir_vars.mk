7bf68fbdd206ad6ed0556dded607ab1d8c70febd
lree <38476310+lree@users.noreply.github.com>
2018-04-24 14:54:10 -0600

    Add files via upload

---
+++ DDL Project/Debug/subdir_vars.mk
@@ -0,0 +1,71 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+SHELL = cmd.exe
+
+# Add inputs and outputs from these tool invocations to the build variables 
+CMD_SRCS += \
+../msp432p401r.cmd 
+
+C_SRCS += \
+../Decoder.c \
+../GPIO.c \
+../bluetoothOn.c \
+../main.c \
+../startup_msp432p401r_ccs.c \
+../system_msp432p401r.c \
+../timer.c \
+../uart.c 
+
+C_DEPS += \
+./Decoder.d \
+./GPIO.d \
+./bluetoothOn.d \
+./main.d \
+./startup_msp432p401r_ccs.d \
+./system_msp432p401r.d \
+./timer.d \
+./uart.d 
+
+OBJS += \
+./Decoder.obj \
+./GPIO.obj \
+./bluetoothOn.obj \
+./main.obj \
+./startup_msp432p401r_ccs.obj \
+./system_msp432p401r.obj \
+./timer.obj \
+./uart.obj 
+
+OBJS__QUOTED += \
+"Decoder.obj" \
+"GPIO.obj" \
+"bluetoothOn.obj" \
+"main.obj" \
+"startup_msp432p401r_ccs.obj" \
+"system_msp432p401r.obj" \
+"timer.obj" \
+"uart.obj" 
+
+C_DEPS__QUOTED += \
+"Decoder.d" \
+"GPIO.d" \
+"bluetoothOn.d" \
+"main.d" \
+"startup_msp432p401r_ccs.d" \
+"system_msp432p401r.d" \
+"timer.d" \
+"uart.d" 
+
+C_SRCS__QUOTED += \
+"../Decoder.c" \
+"../GPIO.c" \
+"../bluetoothOn.c" \
+"../main.c" \
+"../startup_msp432p401r_ccs.c" \
+"../system_msp432p401r.c" \
+"../timer.c" \
+"../uart.c" 
+
+
---
+++ DDL Project/GPIO.h
@@ -0,0 +1,19 @@
+/*
+ * GPIO.h
+ *
+ *  Created on: Apr 19, 2018
+ *      Author: ktbow
+ */
+
+#ifndef GPIO_H_
+#define GPIO_H_
+
+#include "msp.h"
+
+// function prototype for configuring our GPIO pins
+void GPIO_Config();
+
+
+
+
+#endif /* GPIO_H_ */
