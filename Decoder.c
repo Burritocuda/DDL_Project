38e873b106075ecfed8c716cf7030a8b1f6ee203
lree <38476310+lree@users.noreply.github.com>
2018-05-01 11:45:15 -0600

    Add files via upload

--- Decoder.c
+++ Decoder.c
@@ -11,6 +11,8 @@
 #include "string.h"
 
 extern volatile uint8_t * data_array[16];
+extern int status;
+uint8_t enable=1;//lock enable disable parameter
 uint8_t toggle = 0;
 uint8_t user_1[16] = {0x02,0x31,0x44,0x30,0x30,0x41,0x41,0x42,0x30,0x31,0x43,0x31,0x42,0x0D,0x0A,0x03};
 uint8_t user_2[16] = {0x02,0x31,0x44,0x30,0x30,0x41,0x41,0x43,0x35,0x32,0x32,0x35,0x30,0x0D,0x0A,0x03};
@@ -42,7 +44,9 @@ void decoder(void)
     }
     if(u1==15){
         UART_send_n("user_1 accessed device\n", strlen("user_1 accessed device\n"));
-        if(toggle == 0)
+        if(enable ==1){//checks lock ready
+
+            if(toggle == 0)
                   {
                       pwm_up();
                   }
@@ -55,10 +59,13 @@ void decoder(void)
 
                   toggle++;
                   toggle = toggle%2;
+        }
     }
     else if(u2==15){
         UART_send_n("user_2 accessed device\n", strlen("user_2 accessed device\n"));
-        if(toggle == 0)
+        if(enable ==1){//checks lock ready
+
+            if(toggle == 0)
                   {
                       pwm_up();
 
@@ -72,13 +79,67 @@ void decoder(void)
 
                   toggle++;
                   toggle = toggle%2;
+
+        }
     }
     else if(m==15){
         UART_send_n("Master access granted\n", strlen("Master access granted\n"));
-
+        UART_send_n("1. Lock Status\n2. Lock\n3. Unlock\n4. Lock Ready\n5. Enable Lock\n6. Disable Lock", strlen("1. Lock Status\n2. Lock\n3. Unlock\n4. Lock Ready\n5. Enable Lock\n6. Disable Lock"));
+        if(1){
+            if(status==1){
+                UART_send_n("Open",strlen("Open"));
+            }
+            else if(status==0){
+                UART_send_n("Closed",strlen("Closed"));
+            }
+        }
+        else if(2){
+            if(status==0){
+                UART_send_n("Already Locked",strlen("Already Locked"));
+            }
+            else if(status==1){
+                pwm_down();
+                UART_send_n("unlocked",strlen("unlocked"));
+            }
+        }
+        else if(3){
+            if(status==1){
+                          UART_send_n("Already Unlocked",strlen("Already Unlocked"));
+                      }
+                      else if(status==0){
+                          pwm_up();
+                          UART_send_n("locked",strlen("locked"));
+                      }
+        }
+        else if(4){
+            if(enable==1){
+                UART_send_n("Enabled",strlen("Enabled"));
+            }
+            else if(enable==0){
+                UART_send_n("Disabled",strlen("Disabled"));
+            }
+        }
+        else if(5){
+            if(enable==1){
+                UART_send_n("Already enabled",strlen("Already enabled"));
+            }
+            else if(enable==0){
+            enable=1;
+            UART_send_n("enabled",strlen("enabled"));
+            }
+        }
+        else if(6){
+            if(enable==0){
+                        UART_send_n("Already disabled",strlen("Already disabled"));
+                    }
+                    else if(enable==1){
+                    enable=0;
+                    UART_send_n("disabled",strlen("disabled"));
+                    }
+        }
     }
     else{
-        UART_send_n("unknown user accessed the device\n", strlen("unknown user accessed the device\n"));
+        UART_send_n("unknown user tried to access the device\n", strlen("unknown user tried to access the device\n"));
     }
     //reset values to zero
     for(j = 0; j < 15; j++)
